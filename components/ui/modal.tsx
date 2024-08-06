2568a49c98dbdf1dd8b8925f357bf792ab8f81c4
unknown <aa.aityounes.anis@gmail.com>
2024-08-06 20:59:02 +0100

    create a custom modal to create delete update other components

---
+++ components/ui/modal.tsx
@@ -0,0 +1,46 @@
+"use client";
+
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog";
+
+interface ModalProps {
+  title: string;
+  description: string;
+  isOpen: boolean;
+  onClose: () => void;
+  children?: React.ReactNode;
+}
+
+const Modal = ({
+  title,
+  description,
+  isOpen,
+  onClose,
+  children,
+}: ModalProps) => {
+  const onChange = () => {
+    if (!isOpen) {
+      onClose();
+    }
+  };
+
+  return (
+    <Dialog open={isOpen} onOpenChange={onChange}>
+      <DialogTrigger>Open</DialogTrigger>
+      <DialogContent>
+        <DialogHeader>
+          <DialogTitle>{title}</DialogTitle>
+          <DialogDescription>{description}</DialogDescription>
+        </DialogHeader>
+      </DialogContent>
+    </Dialog>
+  );
+};
+
+export default Modal;
