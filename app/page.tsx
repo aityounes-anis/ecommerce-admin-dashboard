868a179fbe128925f78792f0a9d730c9d0bd306c
unknown <aa.aityounes.anis@gmail.com>
2024-08-07 21:51:06 +0100

    Server side component Client side component confusion eliminated

--- app/page.tsx
+++ app/page.tsx
@@ -1,35 +1,22 @@
 import { getAllStores } from "@/actions/get-stores";
-import {
-  Card,
-  CardDescription,
-  CardFooter,
-  CardHeader,
-  CardTitle,
-} from "@/components/ui/card";
+import OpenModal from "@/components/ui/open-modal";
+import StoresList from "@/components/ui/stores-list";
+import { Plus } from "lucide-react";
 
-import Link from "next/link";
+export const revalidate = 0;
 
-export default async function HomePage() {
+const HomePage = async () => {
   const stores = await getAllStores();
 
   return (
     <main className="px-20 mx-auto">
       <h1 className="font-bold text-2xl my-4 mt-8">Stores</h1>
-      <div className="grid grid-cols-2 gap-8 col-span-2">
-        {stores?.map((store) => (
-          <Link key={store.id} href={`/${store?.id}`}>
-            <Card className="cursor-pointer hover:bg-slate-100 transition hover:scale-105">
-              <CardHeader>
-                <CardTitle>{store.name}</CardTitle>
-                <CardDescription>{store.description}</CardDescription>
-              </CardHeader>
-              <CardFooter>
-                <p>{store.createdAt.toString()}</p>
-              </CardFooter>
-            </Card>
-          </Link>
-        ))}
+      <StoresList stores={stores} />
+      <div>
+        <OpenModal btnLabel="Create new Store" logo={<Plus />} />
       </div>
     </main>
   );
-}
+};
+
+export default HomePage;
